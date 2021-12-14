a76b62ae1b509f2f59bb6b646334e3c69f34e305
BrittanyLouviere <brittanynlouviere.gmail.com>
2021-12-14 12:04:44 -0500

    Completed day 14 part 2

--- 2021/Day14/day14.py
+++ 2021/Day14/day14.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import os
+from math import ceil
 
 root = os.path.dirname(os.path.abspath(__file__))
 example = os.path.join(root, 'Example.txt')
@@ -8,23 +9,38 @@ bigboy = os.path.join(root, 'BigBoy.txt')
 
 polymer = ""
 rules = {}
-with open(example, 'r') as f:
+with open(input, 'r') as f:
   polymer = f.readline().strip()
   f.readline()
   for line in f.readlines():
     pair, insert = line.strip().split(" -> ")
     rules[pair] = insert
 
+pairs = {}
+for i in range(1, len(polymer)):
+  pair = polymer[i-1 : i+1]
+  count = pairs.get(pair, 0)
+  pairs[pair] = count + 1
+
 steps = 40
 for step in range(steps):
-  p = ""
-  for i in range(len(polymer)):
-    pair = polymer[i:i+2]
-    mid = rules.get(pair, "")
-    p += pair[0] + mid
-  polymer = p
+  p = {}
+  for pair, oldCount in pairs.items():
+    newChar = rules[pair]
+    for newPair in [pair[0] + newChar, newChar + pair[1]]:
+      count = p.get(newPair, 0)
+      p[newPair] = count + oldCount
+  pairs = p
 
-counts = []
-for char in set(polymer):
-  counts.append(polymer.count(char))
-print(max(counts) - min(counts))
+charCounts = {}
+for pair, pairCount in pairs.items():
+  for char in pair:
+    oldCount = charCounts.get(char, 0)
+    charCounts[char] = oldCount + pairCount
+
+for char, count in charCounts.items():
+  charCounts[char] = int(ceil(count/2))
+if polymer[0] == polymer[-1]:
+  charCounts[polymer[0]] += 1
+
+print(max(charCounts.values()) - min(charCounts.values()))

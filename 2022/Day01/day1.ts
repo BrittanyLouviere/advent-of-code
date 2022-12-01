4e2e6ede323c0c370d58840b01a759e959a3e6e4
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2022-12-01 15:25:24 -0500

    Completed 2022 Day 1 part 2

--- 2022/Day01/day1.ts
+++ 2022/Day01/day1.ts
@@ -4,16 +4,37 @@ let examplefile :string = "example.txt";
 let inputFile :string = "input.txt"
 const input = fs.readFileSync('Day01/' + inputFile,'utf8').split("\r\n\r\n");
 
-let currentMax: number = 0;
-input.forEach(elf => {
-  let foodBag = elf.split("\r\n");
-  let calories :number = 0;
+function part1(){
+  let currentMax: number = 0;
+  input.forEach(elf => {
+    let foodBag = elf.split("\r\n");
+    let calories :number = 0;
 
-  foodBag.forEach(food => {
-    calories += +food;
+    foodBag.forEach(food => {
+      calories += +food;
+    });
+
+    currentMax = calories > currentMax ? calories : currentMax;
   });
 
-  currentMax = calories > currentMax ? calories : currentMax;
-});
+  return currentMax;
+}
 
-console.log(currentMax);
+function part2(){
+  let calorieCounts: number[] = [];
+  input.forEach(elf => {
+    let foodBag = elf.split("\r\n");
+    let calories :number = 0;
+
+    foodBag.forEach(food => {
+      calories += +food;
+    });
+
+    calorieCounts.push(calories);
+  });
+
+  calorieCounts = calorieCounts.sort((n1,n2) => n2 - n1)
+  return calorieCounts[0] + calorieCounts[1] + calorieCounts[2];
+}
+
+console.log(part2());

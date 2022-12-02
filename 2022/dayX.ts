4057b42eca032c0c5f99376c52c7ba8f96bb4d24
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2022-12-02 09:33:00 -0500

    Added template

---
+++ 2022/dayX.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+
+let exampleFile :string = "example.txt";
+let inputFile :string = "input.txt"
+const input = fs.readFileSync('DayXX/' + exampleFile,'utf8').split("\r\n");
+
+function part1(){
+
+}
+
+function part2(){
+
+}
+
+console.log(part1());

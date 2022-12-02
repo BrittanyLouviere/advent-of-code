3370ddd98cf270f6ac5b242a29f3008e790c706b
BrittanyLouviere <67611757+BrittanyLouviere@users.noreply.github.com>
2022-12-02 08:31:58 -0500

    Finished Day 2 part 1

---
+++ 2022/Day02/day2.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs'
+
+let exampleFile :string = "example.txt"
+let inputFile :string = "input.txt"
+const input = fs.readFileSync('Day02/' + inputFile,'utf8').split("\r\n")
+
+function part1(){
+  let totalScore :number = 0
+  input.forEach(round => {
+    let choices :string[] = round.split(' ')
+
+    // get choices
+    let opponent :number = choices[0].charCodeAt(0) - 64
+    let me :number = choices[1].charCodeAt(0) - 87
+
+    // get score for choice used
+    totalScore += me
+
+    // determine win/lose/tie
+    if (opponent == me) { // tie
+      totalScore += 3
+    } else if ((me == 1 && opponent == 3) || (me == 2 && opponent == 1) || (me == 3 && opponent == 2)) { //win
+      totalScore += 6
+    }
+    // nothing if lose
+  });
+  return totalScore
+}
+
+function part2(){
+
+}
+
+console.log(part1())
